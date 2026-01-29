ead950b6e942e4375ce72edb9c7507935d63e220
Harry Chopra <22281143+harrychopra@users.noreply.github.com>
2026-01-29 10:56:00 +0000

    Add seed data and failing tests for seeding db and schema

---
+++ db/seed-data/test/index.js
@@ -0,0 +1,6 @@
+import articles from './articles.js';
+import comments from './comments.js';
+import topics from './topics.js';
+import users from './users.js';
+
+export default { articles, comments, topics, users };
